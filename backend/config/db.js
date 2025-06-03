39b145b17d13a0c54db0dadf82394916d00b1576
Majid Zandinia <majid.zandi.nia@outlook.com>
2025-06-04 00:01:20 +0300

    add logging fo db_uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,12 +7,14 @@ const DBPort = process.env.docdb_port;
 
 const DB_URI = `mongodb://${DBUsername}:${DBPassword}@${DBHost}:${DBPort}/trivia?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
 
+console.log(`DB_URI: ${DB_URI}`);
+
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(DB_URI);
     console.log(`Connected to DocumentDB: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Can't connect to DB: ${error.message}`);
     process.exit(1);
   }
 };
