332114c10e1fb562f6444264f52f685f1ed4a748
Majid Zandinia <majid.zandi.nia@outlook.com>
2025-06-04 00:10:32 +0300

    correct the db variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const DBUsername = process.env.docdb_master_username;
-const DBPassword = process.env.docdb_master_password;
-const DBHost = process.env.docdb_host;
-const DBPort = process.env.docdb_port;
+const DBUsername = process.env.DBUsername;
+const DBPassword = process.env.DBPassword;
+const DBHost = process.env.DBHost;
+const DBPort = process.env.DBPort;
 
 const DB_URI = `mongodb://${DBUsername}:${DBPassword}@${DBHost}:${DBPort}/trivia?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
 
