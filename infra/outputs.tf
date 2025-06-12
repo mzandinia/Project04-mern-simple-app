a4334af71ad563551b3b278aae7a2f2d2621dc08
Majid Zandinia <majid.zandi.nia@outlook.com>
2025-06-12 13:24:25 +0300

    frontend infra

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,3 +8,24 @@ output "cluster_endpoint" {
   description = "The connection endpoint for the DocumentDB cluster"
   value       = module.docdb.cluster_endpoint
 }
+
+# CloudFront outputs
+output "cloudfront_distribution_domain" {
+  description = "The domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.frontend_distribution.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  description = "The ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.frontend_distribution.id
+}
+
+output "frontend_bucket_name" {
+  description = "The name of the S3 bucket hosting the frontend"
+  value       = aws_s3_bucket.frontend_bucket.id
+}
+
+output "frontend_url" {
+  description = "The URL to access the frontend application"
+  value       = "https://${aws_cloudfront_distribution.frontend_distribution.domain_name}"
+}
