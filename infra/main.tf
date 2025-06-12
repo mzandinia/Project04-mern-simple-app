47309c9a7714fdbe84e9756d07e616c1cf55b9e5
Majid Zandinia <majid.zandi.nia@outlook.com>
2025-06-12 10:03:42 +0300

    crete ecr

---
+++ infra/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.1"
+    }
+  }
+
+  backend "s3" {
+  }
+}
